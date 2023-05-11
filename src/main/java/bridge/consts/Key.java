cc38c6aa26f96132676ca32b921ee88ddfc95512
KyuCheol Hwang <97iambigman@gmail.com>
2023-05-11 17:50:39 +0900

    feat(consts/Key) : 입력 가능한 이동 키를 상수화

---
+++ src/main/java/bridge/consts/Key.java
@@ -0,0 +1,11 @@
+package bridge.consts;
+
+import java.util.List;
+
+public class Key {
+
+    public static final String MOVING_UP = "U";
+    public static final String MOVING_DOWN = "D";
+
+    public static final List<String> MOVING_KEYS = List.of(MOVING_UP, MOVING_DOWN);
+}
