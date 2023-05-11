4fc36401410d8c85aa40d2c8d395a500196b2b08
KyuCheol Hwang <97iambigman@gmail.com>
2023-05-11 16:11:57 +0900

    feat(utils/InputValidator) : 다리 길이 입력이 null인지 판단하는 메소드 구현

---
+++ src/main/java/bridge/utils/InputValidator.java
@@ -0,0 +1,11 @@
+package bridge.utils;
+
+public class InputValidator {
+
+    private boolean isNull(String input) {
+        if (input == null) {
+            return true;
+        }
+        return false;
+    }
+}
