3c1585de3dcb42a9b99d67f54f109939535a51a5
KyuCheol Hwang <97iambigman@gmail.com>
2023-05-11 16:16:09 +0900

    style(utils/InputValidator) : 메소드에 주석 추가

--- src/main/java/bridge/utils/InputValidator.java
+++ src/main/java/bridge/utils/InputValidator.java
@@ -2,6 +2,11 @@ package bridge.utils;
 
 public class InputValidator {
 
+    /**
+     * 입력이 null 인지 판단하는 메소드
+     * @param input
+     * @return boolean
+     */
     private boolean isNull(String input) {
         if (input == null) {
             return true;
@@ -9,6 +14,11 @@ public class InputValidator {
         return false;
     }
 
+    /**
+     * 입력이 숫자 인지 판단하는 메소드
+     * @param input
+     * @return boolean
+     */
     private boolean isInteger(String input) {
         try {
             Integer.parseInt(input);
