d34968d5e98f52e5f7725cc1ffebc4704c869936
KyuCheol Hwang <97iambigman@gmail.com>
2023-05-12 16:19:41 +0900

    feat(consts/Command) : 입력 가능한 명령어를 상수화

---
+++ src/main/java/bridge/consts/Command.java
@@ -0,0 +1,11 @@
+package bridge.consts;
+
+import java.util.List;
+
+public class Command {
+
+    public static final String RETRY = "R";
+    public static final String QUIT = "Q";
+
+    public static final List<String> COMMANDS = List.of(RETRY, QUIT);
+}
